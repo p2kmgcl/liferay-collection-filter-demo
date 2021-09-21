aed7164d347ee45bdd3bbeda07fa422aab0484df
Victor Galan <victor.galan@liferay.com>
2021-09-21 15:57:16 +0200

    Add filter and provider

---
+++ modules/date-filter/src/main/java/com/liferay/date/filter/DateInfoFilterProvider.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.date.filter;
+
+import com.liferay.info.filter.InfoFilterProvider;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+/**
+ * @author Víctor Galán
+ */
+public class DateInfoFilterProvider implements InfoFilterProvider<InfoFilterDate> {
+
+	@Override
+	public InfoFilterDate create(Map<String, String[]> values) {
+		InfoFilterDate infoFilterDate = new InfoFilterDate();
+
+		for (Map.Entry<String, String[]> entry : values.entrySet()) {
+			if (!StringUtil.startsWith(
+				entry.getKey(),
+				InfoFilterDate.FILTER_TYPE_NAME + "_")) {
+
+				try {
+					infoFilterDate.setDate(_format.parse(entry.getValue()[0]));
+				}
+				catch (ParseException e) {
+					System.out.println("Invalid date format");
+				}
+			}
+		}
+
+		return infoFilterDate;
+	}
+
+	private static final DateFormat _format = new SimpleDateFormat("yyyy-MM-dd");
+}
--- modules/date-filter/src/main/java/com/liferay/date/filter/InfoFilterDate.java
+++ modules/date-filter/src/main/java/com/liferay/date/filter/InfoFilterDate.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -16,14 +16,29 @@ package com.liferay.date.filter;
 
 import com.liferay.info.filter.InfoFilter;
 
+import java.util.Date;
+import java.util.Optional;
+
 /**
  * @author Pablo Molina
  */
 public class InfoFilterDate implements InfoFilter {
 
+	public static final String FILTER_TYPE_NAME = "date";
+
+	public Optional<Date> getDateOptional() {
+		return Optional.ofNullable(_date);
+	}
+
 	@Override
 	public String getFilterTypeName() {
-		return "date";
+		return FILTER_TYPE_NAME;
 	}
+
+	public void setDate(Date date) {
+		_date = date;
+	}
+
+	private Date _date;
 
 }
